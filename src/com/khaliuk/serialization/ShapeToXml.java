8b40c9c6fe7232f60b48580fbe528cb7ee383bd6
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2019-01-25 18:36:22 +0200

    Remove unnecessary line

--- src/com/khaliuk/serialization/ShapeToXml.java
+++ src/com/khaliuk/serialization/ShapeToXml.java
@@ -3,9 +3,7 @@ package com.khaliuk.serialization;
 public class ShapeToXml {
     public static String encode(Shape shape) {
         String xmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-        if (shape instanceof Triangle) {
-            xmlString += shape.encodeToXml();
-        }
+        xmlString += shape.encodeToXml();
         return xmlString;
     }
 }
